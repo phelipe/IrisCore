4d3f091c9e13dfa9217ffc1057126e6bb2a2dfff
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2020-05-29 14:40:45 -0300

    update gitignore

--- src/IrisCore.jl
+++ src/IrisCore.jl
@@ -1,14 +1,15 @@
-module Iriscore
+module IrisCore
 
 using MLJ:machine
+using MLJ:predict_mode
 using DataFrames
 using DecisionTree
 
-mach = machine("modelo.jlso")
+mach = machine("src/modelo.jlso")
 
 export predictiris
 
-function predictiris(in1::T, in2::T, in3::T) where T::Float64
+function predictiris(in1::T, in2::T, in3::T) where {T<:Float64}
     predict_mode( mach, DataFrame([[in1],[in2],[in3]],
                  [:sepallength,:sepalwidth,:petallength]))
 end
